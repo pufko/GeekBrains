a852016974d094107425d51470ba1edb07c7d72c
pufko <pufan4eg@gmail.com>
2017-02-23 05:29:34 +0200

    First lessons done!

---
+++ L1/Main.java
@@ -0,0 +1,15 @@
+package L1;
+
+/**
+ * Created by mr p on 23.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int i = 2000000000;
+        System.out.println(Integer.MAX_VALUE);
+        short sh = 32000;
+        System.out.println(Short.MAX_VALUE);
+        long l = 211111155555555555L;
+        System.out.println(Long.MAX_VALUE);
+    }
+}
