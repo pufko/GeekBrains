c61860d555e5795fff9e43fc5d5dbea327a46f93
pufko <pufan4eg@gmail.com>
2017-02-23 14:43:10 +0200

    Arrays

--- L1/Array2.java
+++ L1/Array2.java
@@ -1,7 +1,22 @@
 package L1;
 
+import java.util.Arrays;
+
 /**
  * Created by mr p on 23.02.2017.
  */
 public class Array2 {
-}
+    public static void main(String[] args) {
+
+        int a;
+        a = 10;
+        int[] numbers;
+        numbers = new int[100];
+//           for (int number : numbers) {
+            numbers [5] = 10;
+//           System.out.println(number);
+            System.out.println(Arrays.toString(numbers));
+        }
+    }
+
+
