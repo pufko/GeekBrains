3ad56316cc7f1bd6a37d40ce8e2cbb5f6562117a
pufko <pufan4eg@gmail.com>
2017-03-02 08:04:54 +0200

    McGrat

---
+++ McGrat/Exceptions.java
@@ -0,0 +1,19 @@
+package McGrat;
+
+/**
+ * Created by mr p on 02.03.2017.
+ */
+public class Exceptions {
+    public static void main(String[] args) {
+        try {
+            int num = Integer.parseInt(args[0]);
+            System.out.println("Вы ввели: " + num);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Требуется целочисленный аргумент.");
+        } catch (NumberFormatException e) {
+            System.out.println("Неверный формат аргумента.");
+        } finally {
+            System.out.println("Программа завершила работу.");
+        }
+    }
+}
