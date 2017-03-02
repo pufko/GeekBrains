e8c31d223cbbddbd470f7ff74efc5dc982376891
pufko <pufan4eg@gmail.com>
2017-03-02 04:24:48 +0200

    McGrat

---
+++ L1/Assignment.java
@@ -0,0 +1,25 @@
+package L1;
+
+/**
+ * Created by mr p on 02.03.2017.
+ */
+public class Assignment {
+    public static void main(String[] args) {
+        String txt = "Fantastic";
+        String lang = "Java" ;
+        txt += lang ; // Присваивание с объединением строк
+        System.out.println( "Складываем и присваиваем строки: " + txt ) ;
+
+        int sum = 10 ;
+        int num = 20 ;
+        sum += num ; // Присваиваем результат ( 10 + 20 = 30 )
+        System.out.println( " Складываем и присваиваем целые числа: " + sum ) ;
+
+        int factor = 5 ;
+        sum *= factor ; // Присваиваем результат ( 30 x 5 = 150 )
+        System.out.println( "Результат умножения " + sum ) ;
+
+        sum /= factor ; // Присваиваем результат ( 150 ÷ 5 = 30 )
+        System.out.println( " Результат деления: " + sum ) ;
+    }
+}
